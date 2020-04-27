c088685f865f727e97988b9c514624da811e2130
Danish Khan <danishkhan@zapbuild.com>
2020-04-27 17:32:00 +0530

    Initial Commit

---
+++ Allday.com/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Allday.com
+//
+//  Created by Danish Khan on 27/04/20.
+//  Copyright © 2020 Danish Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
